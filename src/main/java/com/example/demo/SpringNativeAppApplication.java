8d9a389ec6907a32b71658116809d5f797db78dd
shenlanAZ <shenlanluck@gmail.com>
2021-07-07 09:38:25 +0800

    initial

---
+++ src/main/java/com/example/demo/SpringNativeAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNativeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringNativeAppApplication.class, args);
+	}
+
+}
